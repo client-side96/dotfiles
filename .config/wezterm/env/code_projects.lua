9cd6784e35729183bd1093574956886742d5a37d
Domi <34790329+client-side96@users.noreply.github.com>
2024-07-21 15:27:15 +0200

    refactor(wezterm): align projects

--- .config/wezterm/env/code_projects.lua
+++ .config/wezterm/env/code_projects.lua
@@ -2,10 +2,16 @@ local wezterm = require 'wezterm'
 
 local project_dir = wezterm.home_dir .. '/devel'
 
-local locksmith_dir = wezterm.home_dir .. '/clientside/locksmith'
+local mobile_app_dir = project_dir .. '/care/app-mobile'
+local middleware_dir = project_dir .. '/care/app-middleware'
+local virtual_coach_dir = project_dir .. '/care/virtual-coach-app'
+local deployment_dir = project_dir .. '/care/app-middleware-deployment'
 
 local code_projects = {
-  ["locksmith"] = locksmith_dir,
+  ["care-mobile"] = mobile_app_dir,
+  ["care-middleware"] = middleware_dir,
+  ["care-va"] = virtual_coach_dir,
+  ["care-deployment"] = deployment_dir,
 }
 
 return code_projects
