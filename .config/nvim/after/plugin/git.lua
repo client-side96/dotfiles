ed4ab9333f8b8913acfea42a76b8b0ef74bd3d8c
Dominic Frey <dev@clientside.tech>
2025-02-06 21:41:36 +0100

    feat: add oil file explorer and toggle term for git docker and monitoring

---
+++ .config/nvim/after/plugin/git.lua
@@ -0,0 +1,72 @@
+local gitsigns = require("gitsigns")
+local Terminal = require("toggleterm.terminal").Terminal
+
+gitsigns.setup()
+
+local lazygit_cmd = Terminal:new({
+	cmd = "lazygit",
+	hidden = true,
+	direction = "float",
+	float_opts = {
+		width = function()
+			return math.floor(vim.o.columns * 0.95)
+		end,
+		height = function()
+			return math.floor(vim.o.lines * 0.95)
+		end,
+	},
+})
+
+local lazydocker_cmd = Terminal:new({
+	cmd = "lazydocker",
+	hidden = true,
+	direction = "float",
+	float_opts = {
+		width = function()
+			return math.floor(vim.o.columns * 0.95)
+		end,
+		height = function()
+			return math.floor(vim.o.lines * 0.95)
+		end,
+	},
+})
+
+local top_cmd = Terminal:new({
+	cmd = "btm",
+	hidden = true,
+	direction = "float",
+	float_opts = {
+		width = function()
+			return math.floor(vim.o.columns * 0.95)
+		end,
+		height = function()
+			return math.floor(vim.o.lines * 0.95)
+		end,
+	},
+})
+
+function Lazygit_toggle()
+	lazygit_cmd:toggle()
+end
+
+function Lazydocker_toggle()
+	lazydocker_cmd:toggle()
+end
+
+function Top_toggle()
+	top_cmd:toggle()
+end
+
+vim.api.nvim_set_keymap("n", "<leader>gg", "<cmd>lua Lazygit_toggle()<CR>", {
+	noremap = true,
+	silent = true,
+})
+
+vim.api.nvim_set_keymap("n", "<leader>md", "<cmd>lua Lazydocker_toggle()<CR>", {
+	noremap = true,
+	silent = true,
+})
+vim.api.nvim_set_keymap("n", "<leader>mm", "<cmd>lua Top_toggle()<CR>", {
+	noremap = true,
+	silent = true,
+})
---
+++ .config/nvim/after/plugin/oil.lua
@@ -0,0 +1,23 @@
+local oil = require("oil")
+oil.setup({
+	columns = {
+		"icon",
+		"size",
+	},
+	watch_for_changes = true,
+	keymaps = {
+		["q"] = { "actions.close", mode = "n" },
+	},
+	view_options = {
+		show_hidden = true,
+		is_always_hidden = function(name, bufnr)
+			return name == "node_modules" or name == ".git" or name == ".DS_Store"
+		end,
+	},
+	float = {
+		max_width = 0.3,
+		max_height = 0.5,
+	},
+})
+
+vim.keymap.set("n", "<leader>n", ":Oil --float<cr>")
