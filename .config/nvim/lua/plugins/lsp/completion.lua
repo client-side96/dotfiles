52e7486e69958c0b0bffe2e280a55d985cd95f7c
Dominic Frey <tech@dominicfrey.dev>
2025-04-24 08:00:29 +0200

    refactor: use blink cmp instead of nvim cmp

--- .config/nvim/lua/plugins/lsp/completion.lua
+++ .config/nvim/lua/plugins/lsp/completion.lua
@@ -1,51 +1,13 @@
 local completion = {}
 
 function completion.setup()
-	local cmp = require("cmp")
+	require("blink.cmp").setup({
+		completion = { documentation = { auto_show = true } },
+		keymap = {
+			preset = "default",
 
-	cmp.setup({
-		sources = {
-			{ name = "nvim_lsp", group_index = 2 },
-		},
-		mapping = {
-			["<C-e>"] = cmp.mapping.close(),
-			["<CR>"] = cmp.mapping.confirm({
-				behavior = cmp.ConfirmBehavior.Replace,
-				select = false,
-			}),
-			["<Tab>"] = cmp.mapping(function(fallback)
-				if cmp.visible() then
-					cmp.select_next_item()
-				else
-					fallback()
-				end
-			end, { "i", "s" }),
-			["<S-Tab>"] = cmp.mapping(function(fallback)
-				if cmp.visible() then
-					cmp.select_prev_item()
-				else
-					fallback()
-				end
-			end, { "i", "s" }),
-		},
-		window = {
-			completion = {
-				side_padding = 0,
-			},
-		},
-		formatting = {
-			format = require("lspkind").cmp_format({
-				mode = "symbol",
-				maxwidth = {
-					menu = 50,
-					abbr = 50,
-				},
-				ellipsis_char = "...",
-
-				before = function(entry, vim_item)
-					return vim_item
-				end,
-			}),
+			["<Tab>"] = { "select_next", "fallback" },
+			["<S-Tab>"] = { "select_prev", "fallback" },
 		},
 	})
 end
