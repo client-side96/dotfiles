2da7118b0e0307e8af32748f5a76847b4a1ba8f7
Dominic Frey <tech@dominicfrey.dev>
2025-05-27 14:32:18 +0200

    feat: configure code companion

--- .config/nvim/lua/plugins/lsp/completion.lua
+++ .config/nvim/lua/plugins/lsp/completion.lua
@@ -10,6 +10,16 @@ function completion.setup()
 			["<S-Tab>"] = { "select_prev", "fallback" },
 			["<CR>"] = { "accept", "fallback" },
 		},
+		sources = {
+			default = { "lazydev", "lsp", "path", "buffer" },
+			providers = {
+				lazydev = {
+					name = "LazyDev",
+					module = "lazydev.integrations.blink",
+					score_offset = 100,
+				},
+			},
+		},
 	})
 end
 
